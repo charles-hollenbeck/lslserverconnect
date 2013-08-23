8b43eacb096805e60349ca911078976621291e0f
Charles Hollenbeck <charles@hollenbeck.pw>
2013-08-23 18:10:13 -0400

    First Major Push

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
 
-get '/' do
-    "Hello, Zach"
+class Servers
+    include Mongoid::Document
+    field :server_name, type :String
+    field :uri, type: String
+    field :creation_date, type: Date
+    field :last_reset, type: Date
+    field :last_request, type: DateTime
+end
+
+class API_Key
+    include Mongo::Document
+    field :user, type String
+    field :key, type String
+end
+
+post '/set/:server' do
+    #Wanting to set a server
+end
+
+get '/get/:server' do
+    server = Servers.where(server_name: :server)
+    if server.empty? do
+        "BOOOOO IT WORKS"
+    end
 end
